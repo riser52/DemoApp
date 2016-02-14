060c90d70288214350c479cd3d537ca63bd08d32
riser52 <duggapuravikumar@gmail.com>
2016-02-14 13:52:02 -0500

    Delete templates.js

--- cont/WebContent/assets/js/templates.js
+++
@@ -1,7 +0,0 @@
-/**
- * DO NOT Add anything here. This file is used to put all templates by build system
- */
-define(['angular'],function(angular){
-	//DO NOT Add anything here. This file is used to put all templates by build system
-	angular.module('templates', []);
-})
